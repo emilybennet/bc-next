7e570f91e97d7c2550ae36259cfb09b14020e0fd
Emily Bennet <me@emilybennet.com>
2018-12-03 17:27:04 -0500

    liven up the error page a smidge

--- pages/_error.js
+++ pages/_error.js
@@ -1,3 +1,12 @@
-const Error = props => <div>Aw heck..</div>;
+import BasicLayout from "../components/BasicLayout";
+import Directive from "../components/Directive";
+import SectionHeading from "../components/styled/SectionHeading";
+
+const Error = props => (
+  <BasicLayout>
+    <Directive>¿¿??¿?¿¿ ¿¿??¿¿¿¿ ¿¿??¿?¿¿</Directive>
+    <SectionHeading text="404" />
+  </BasicLayout>
+);
 
 export default Error;
