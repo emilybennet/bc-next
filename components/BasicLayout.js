a5a383448610b16cc5d13421e4d32f1139bc47b0
Emily Bennet <me@emilybennet.com>
2018-12-03 23:15:18 -0500

    Opengraph and favicon

--- components/BasicLayout.js
+++ components/BasicLayout.js
@@ -3,17 +3,21 @@ import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const BasicLayout = props => (
-  <React.Fragment>
-    <Head>
-      <title>
-        {props.pageTitle ? `${props.pageTitle} • ` : ""} BronyCon 2019
-      </title>
-    </Head>
-    <Header />
-    {props.children}
-    <Footer />
-  </React.Fragment>
-);
+const BasicLayout = props => {
+  const titleString = `${
+    props.pageTitle ? props.pageTitle + " •" : ""
+  } BronyCon 2019`;
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{titleString}</title>
+        <meta property="og:title" content={titleString} />
+      </Head>
+      <Header />
+      {props.children}
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default BasicLayout;
