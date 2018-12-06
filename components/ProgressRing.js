a3864b384f4a74bfa2eef313bbe2ff97cd3d1c7f
Emily Bennet <me@emilybennet.com>
2018-12-06 00:25:37 -0500

    retweaked

--- components/ProgressRing.js
+++ components/ProgressRing.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const StyledCircle = styled.circle`
@@ -17,76 +17,68 @@ const StatusText = styled.span`
   text-align: center;
 `;
 
-class ProgressRing extends Component {
-  constructor(props) {
-    super(props);
+const ProgressRing = ({
+  radius,
+  stroke,
+  progress,
+  barColor,
+  Icon,
+  countdown,
+  message
+}) => {
+  const normalizedRadius = radius - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const gridCenterStyles = { gridRow: 1, gridColumn: 1 };
 
-    const { radius, stroke, barColor, Icon } = this.props;
+  let strokeDashoffset = circumference - (progress / 100) * circumference;
+  if (countdown === true) strokeDashoffset = strokeDashoffset * -1;
 
-    this.normalizedRadius = radius - stroke * 2;
-    this.circumference = this.normalizedRadius * 2 * Math.PI;
-  }
-
-  render() {
-    const { radius, stroke, barColor, Icon, progress } = this.props;
-
-    const gridCenterStyles = { gridRow: 1, gridColumn: 1 };
-
-    let strokeDashoffset =
-      this.circumference - (progress / 100) * this.circumference;
-
-    if (this.props.countdown === true) strokeDashoffset = strokeDashoffset * -1;
-
-    let statusText, symbolColor;
-    if (this.props.progress > 0) {
-      statusText = false;
-      symbolColor = this.props.barColor;
-    } else {
-      statusText = (
-        <StatusText style={gridCenterStyles}>{this.props.message}</StatusText>
-      );
-      symbolColor = "rgba(255,255,255,.05)";
-    }
-
-    return (
-      <div
-        style={{
-          display: "grid",
-          alignItems: "center",
-          justifyItems: "center"
-        }}
-      >
-        <svg
-          className=""
-          height={radius * 2}
-          width={radius * 2}
-          style={gridCenterStyles}
-        >
-          <circle
-            stroke="rgba(255,255,255,.05)"
-            fill="transparent"
-            strokeWidth={stroke}
-            r={this.normalizedRadius}
-            cx={radius}
-            cy={radius}
-          />
-          <StyledCircle
-            stroke={barColor}
-            fill="transparent"
-            strokeWidth={stroke}
-            strokeDasharray={this.circumference + " " + this.circumference}
-            style={{ strokeDashoffset }}
-            strokeLinecap="round"
-            r={this.normalizedRadius}
-            cx={radius}
-            cy={radius}
-          />
-        </svg>
-        <Icon fill={symbolColor} style={gridCenterStyles} />
-        {statusText}
-      </div>
+  const symbolColor = progress > 0 ? barColor : "rgba(255,255,255,.05)";
+  const statusText =
+    progress > 0 ? (
+      ""
+    ) : (
+      <StatusText style={gridCenterStyles}>{message}</StatusText>
     );
-  }
-}
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        alignItems: "center",
+        justifyItems: "center"
+      }}
+    >
+      <svg
+        className=""
+        height={radius * 2}
+        width={radius * 2}
+        style={gridCenterStyles}
+      >
+        <circle
+          stroke="rgba(255,255,255,.05)"
+          fill="transparent"
+          strokeWidth={stroke}
+          r={normalizedRadius}
+          cx={radius}
+          cy={radius}
+        />
+        <StyledCircle
+          stroke={barColor}
+          fill="transparent"
+          strokeWidth={stroke}
+          strokeDasharray={`${circumference} ${circumference}`}
+          style={{ strokeDashoffset }}
+          strokeLinecap="round"
+          r={normalizedRadius}
+          cx={radius}
+          cy={radius}
+        />
+      </svg>
+      <Icon fill={symbolColor} style={gridCenterStyles} />
+      {statusText}
+    </div>
+  );
+};
 
 export default ProgressRing;
