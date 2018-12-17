35854683bf3909087aa9ca39d5608ab7233f9c36
Emily Bennet <me@emilybennet.com>
2018-12-17 18:56:37 -0500

    update redirects

--- redirects.js
+++ redirects.js
@@ -6,5 +6,21 @@ module.exports = [
   {
     from: "/about/policies/general-rules",
     to: "/policies"
+  },
+  {
+    from: "/about/policies/press-policy",
+    to: "/policies/press"
+  },
+  {
+    from: "/about/policies/marketplace-policies",
+    to: "/policies/marketplace"
+  },
+  {
+    from: "/marketplace",
+    to: "/exhibit"
+  },
+  {
+    from: "/marketplace/apply-to-sell",
+    to: "/exhibit"
   }
 ];
