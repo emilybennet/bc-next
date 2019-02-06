6a9d7f008a7523bab3c2020874833f065c750664
Lauren Herda <lauren@laurenherda.com>
2019-02-06 03:27:32 -0500

    GuestGrid tweaks

--- components/GuestGrid.js
+++ components/GuestGrid.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Button from "./styled/Button";
 import SectionHeading from "./styled/SectionHeading";
 import DossierCard from "./DossierCard";
+import Directive from "./Directive";
 
 const Container = styled.section`
   display: block;
@@ -13,20 +14,22 @@ const Container = styled.section`
 
 const Lineup = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 40px 20px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: 1fr;
+  grid-gap: 30px;
+  margin: 2em 0 0 0;
 `;
 
 const GUESTS_CONTENT = [
   {
-    photo: "/static/images/guests/nicole-oliver.jpg",
-    name: "Nicole Oliver"
+    photo: "/static/images/guests/placeholders/Curtis.jpg"
   },
   {
     photo: "/static/images/guests/placeholders/Alex.jpg"
   },
   {
-    photo: "/static/images/guests/placeholders/Curtis.jpg"
+    photo: "/static/images/guests/nicole-oliver.jpg",
+    name: "Nicole Oliver"
   },
   {
     photo: "/static/images/guests/placeholders/Jason.jpg"
@@ -38,18 +41,24 @@ const GUESTS_CONTENT = [
     photo: "/static/images/guests/placeholders/Whitney.jpg"
   },
   {
-    photo: "/static/images/guests/britt-mckillip.jpg",
-    name: "Britt McKillip"
+    photo: "/static/images/guests/placeholders/Tyler.jpg"
   },
   {
-    photo: "/static/images/guests/cathy-weseluck.jpg",
-    name: "Cathy Weseluck"
-  }
+    photo: "/static/images/guests/placeholders/Kris.jpg"
+  },
+  // {
+  //   photo: "/static/images/guests/britt-mckillip.jpg",
+  //   name: "Britt McKillip"
+  // },
+  // {
+  //   photo: "/static/images/guests/cathy-weseluck.jpg",
+  //   name: "Cathy Weseluck"
+  // }
 ];
 
 const GuestTease = props => (
   <Container>
-    <SectionHeading text="High-Value Assets" id="guests" />
+    <Directive>Encounter High-Value Assets</Directive>
     <Lineup>
       {GUESTS_CONTENT.map(guest => (
         <DossierCard 
