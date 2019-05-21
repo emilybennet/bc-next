a44a214e628afd89b3a0ea054563df0d402efc60
Emily Bennet <me@emilybennet.com>
2019-05-21 16:41:23 -0400

    update refactored locations.

--- pages/redeem.js
+++ pages/redeem.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-import BasicLayout from "../components/BasicLayout";
-import Directive from "../components/Directive";
-import SectionHeading from "../components/styled/SectionHeading";
+import BasicLayout from "../components/common/BasicLayout";
+import Directive from "../components/common/Directive";
+import SectionHeading from "../components/common/SectionHeading";
 
 const RedeemLayout = styled.section`
   margin: 0 auto;
