0a1b3e73c23cf46a2d0b24818e5a9b3f2b5a06b5
Emily Bennet <me@emilybennet.com>
2019-06-14 15:22:25 -0400

    dynamic policy page titles.

--- components/Policy/index.js
+++ components/Policy/index.js
@@ -5,7 +5,7 @@ import Directive from "../common/Directive";
 import PolicyView from "./PolicyView";
 
 const PolicyPage = ({ pageTitle, Content }) => (
-  <BasicLayout pageTitle="Art Show Policies">
+  <BasicLayout pageTitle={pageTitle}>
     <Directive>Priceless Artifact Protocol</Directive>
     <PolicyView heading="policies" Content={Content} />
   </BasicLayout>
