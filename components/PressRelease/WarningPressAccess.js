78c75811e7ddd0e0f77a77f4c9e37d4f0133cb62
Emily Bennet <me@emilybennet.com>
2019-07-03 16:11:40 -0400

    remove press access notice.

--- components/PressRelease/WarningPressAccess.js
+++ components/PressRelease/WarningPressAccess.js
@@ -2,15 +2,16 @@ import React from "react";
 import Warning from "../common/Warning";
 
 const WarningPressAccess = props => (
-  <Warning
-    actions={[
-      { text: "Review Press Policy", url: "/policies/press" },
-      { text: "Apply For Press Access", url: "http://bit.ly/2BouuoZ" }
-    ]}
-  >
-    Media organizations looking to cover BronyCon 2019 can apply for
-    complementary press badges until June 30, 2019.
-  </Warning>
+  // <Warning
+  //   actions={[
+  //     { text: "Review Press Policy", url: "/policies/press" },
+  //     { text: "Apply For Press Access", url: "http://bit.ly/2BouuoZ" }
+  //   ]}
+  // >
+  //   Media organizations looking to cover BronyCon 2019 can apply for
+  //   complementary press badges until June 30, 2019.
+  // </Warning>
+  <div />
 );
 
 export default WarningPressAccess;
