ca45d8a2c9aec52b17b1a9ed5ae10caf640b20ed
Emily Bennet <me@emilybennet.com>
2019-07-10 21:27:31 -0400

    adjust line up; add times; clump super band; redact one musician.

--- components/BronyPalooza/Lineup.js
+++ components/BronyPalooza/Lineup.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import BasicLayout from "../common/BasicLayout";
 import Directive from "../common/Directive";
 
+import RedactedName from "../../static/images/bp-redacted-name.svg";
+
 const PerformerList = styled.ul`
   width: 100%;
   padding: 0;
@@ -11,9 +13,10 @@ const PerformerList = styled.ul`
 `;
 
 const Performer = styled.li`
-  height: 50vh;
+  min-height: 50vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
@@ -21,61 +24,170 @@ const Performer = styled.li`
   font-size: 3em;
   font-style: italic;
   text-transform: uppercase;
-  letter-spacing: .5em;
+  letter-spacing: 0.5em;
+
+  span {
+    display: block;
+  }
+
+  svg {
+    height: 40px;
+    margin-bottom: 0.25em;
+  }
+
+  path {
+    fill: ${props => props.theme.white};
+  }
 
   @media (max-width: 1300px) {
     width: auto;
-    height: 3em;
+    min-height: 3em;
     font-size: 2em;
-    letter-spacing: .15em;
+    letter-spacing: 0.15em;
+
+    svg {
+      height: 15px;
+      margin-bottom: 0.25em;
+    }
   }
 `;
 
-const PerformerHeader = styled.li`
-  height: 50vh;
-  width: 100vw;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  margin: 0;
-  font-size: 2em;
+const SuperBandPerformer = styled.span`
+  min-height: 15vh;
+`;
+
+const SetTime = styled.span`
+  color: ${props => props.theme.gold};
+  display: block;
+  font-size: 0.25em;
+  letter-spacing: 1em;
+`;
+
+const PerformerHeader = styled.span`
+  display: block;
+  margin: 4em;
+  font-size: 0.5em;
   font-weight: bold;
   text-transform: uppercase;
-  letter-spacing: .15em;
+  letter-spacing: 0.4em;
+  /* width: 100%: */
 
   @media (max-width: 1300px) {
     width: auto;
-    height: 6em;
+    /* height: 6em; */
+    margin-bottom: 1em;
     font-size: 1.4em;
-    letter-spacing: .15em;
+    letter-spacing: 0.15em;
+    font-size: 0.5em;
   }
 `;
+
+// const PerformerHeader = styled.li`
+//   height: 50vh;
+//   width: 100vw;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   text-align: center;
+//   margin: 0;
+//   font-size: 2em;
+//   font-weight: bold;
+//   text-transform: uppercase;
+//   letter-spacing: 0.15em;
+
+//   @media (max-width: 1300px) {
+//     width: auto;
+//     height: 6em;
+//     font-size: 1.4em;
+//     letter-spacing: 0.15em;
+//   }
+// `;
 
 const Lineup = props => (
   <PerformerList>
-    <Performer>Hollowpoint</Performer>
-    <Performer>Seventh Element</Performer>
-    <Performer>John Kenza</Performer>
-    <Performer>YourEnigma</Performer>
-    <Performer>Geek</Performer>
-    <Performer>Loophoof</Performer>
-    <Performer>Elevative</Performer>
-    <Performer>FLIGHTRUSH</Performer>
-    <Performer>R3CTIFIER</Performer>
-    <Performer>Chang31ing</Performer>
-    <Performer>Strach</Performer>
-    <Performer>VinylTastic</Performer>
-    <Performer>Vylet</Performer>
-    <Performer>Garnika</Performer>
-    <Performer>Silva Hound</Performer>
-    <PerformerHeader>Plus the BronyPalooza Superband, featuring: </PerformerHeader>
-    <Performer>Luck Rock</Performer>
-    <Performer>Crusader</Performer>
-    <Performer>4everfree brony </Performer>
-    <Performer>Cyril</Performer>
-    <Performer>P1K</Performer>
-    <Performer>The Wonderbolts</Performer>
+    <Performer>
+      Hollowpoint
+      <SetTime>Thursday @ 7:00pm</SetTime>
+    </Performer>
+    <Performer>
+      Seventh Element
+      <SetTime>Thursday @ 7:40pm</SetTime>
+    </Performer>
+    <Performer>
+      John Kenza
+      <SetTime>Thursday @ 8:20pm</SetTime>
+    </Performer>
+    <Performer>
+      YourEnigma
+      <SetTime>Thursday @ 8:55pm</SetTime>
+    </Performer>
+    <Performer>
+      GeekBrony
+      <SetTime>Thursday @ 9:30pm</SetTime>
+    </Performer>
+    <Performer>
+      loophoof
+      <SetTime>Thursday @ 10:05pm</SetTime>
+    </Performer>
+    <Performer>
+      Elevative
+      <SetTime>Thursday @ 10:40pm</SetTime>
+    </Performer>
+    <Performer>
+      FLIGHTRUSH
+      <SetTime>Thursday @ 11:15pm</SetTime>
+    </Performer>
+    <Performer>
+      R3CTIFIER
+      <SetTime>Thursday @ 11:50pm</SetTime>
+    </Performer>
+    <Performer>
+      Chang31ing
+      <SetTime>Friday @ 12:25am</SetTime>
+    </Performer>
+    <Performer>
+      StrachAttack
+      <SetTime>Friday @ 1:00am</SetTime>
+    </Performer>
+    <Performer>
+      VinylTastic
+      <SetTime>Friday @ 11:00pm</SetTime>
+    </Performer>
+    <Performer>
+      Vylet Pony
+      <SetTime>Friday @ 11:35pm</SetTime>
+    </Performer>
+    <Performer>
+      <RedactedName />
+      <SetTime>Saturday @ 12:10am</SetTime>
+    </Performer>
+    <Performer>
+      Silva Hound
+      <SetTime>Saturday @ 1:00am</SetTime>
+    </Performer>
+    <Performer>
+      Michelle Creber
+      <br />
+      Black Gryph0n,
+      <br />
+      &amp; Baasik
+      <SetTime>Saturday @ 8:00pm</SetTime>
+    </Performer>
+
+    <Performer>
+      <PerformerHeader>The BronyPalooza Superband, featuring:</PerformerHeader>
+      <span>Luck Rock</span>
+      <span>Crusader!</span>
+      <span>Cyril the Wolf</span>
+      <span>P1K</span>
+      <span>The Wonderbolts!</span>
+      <SetTime>Saturday @ 9:40pm</SetTime>
+    </Performer>
+
+    <Performer>
+      Eurobeat Brony
+      <SetTime>Sunday @ 1:00am</SetTime>
+    </Performer>
   </PerformerList>
 );
 
