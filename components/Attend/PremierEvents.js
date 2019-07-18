e95a8c13257fdce3ceb6a6bab6eed38f4dccc95f
Emily Bennet <me@emilybennet.com>
2019-07-17 23:59:30 -0400

    support for sold out items.

--- components/Attend/PremierEvents.js
+++ components/Attend/PremierEvents.js
@@ -64,11 +64,21 @@ const StyledEvent = styled.li`
       padding: 20px 30px;
       text-transform: uppercase;
       transition: color 0.15s;
-      &:hover {
+      &:hover:not(.disabled) {
         color: ${props => props.theme.gold};
       }
       &:after {
         content: " >";
+      }
+      &.disabled {
+        color: rgba(57, 81, 89, 1);
+        cursor: default;
+        text-decoration: line-through;
+        user-select: none;
+        &:focus {
+          box-shadow: 0 0 0 1px ${props => props.theme.midnight},
+            0 0 0 4px ${props => props.theme.ruby};
+        }
       }
       &:not(:last-child) {
         border-right: 1px solid ${props => props.theme.turquoise};
@@ -98,8 +108,10 @@ const PREMIER_EVENTS = [
     loc: "Friday @ 9am, 2pm, 4pm – Hall of the Stars",
     controls: [
       {
-        text: "Buy Tickets",
-        url: "/cafe"
+        // text: "Buy Tickets",
+        text: "Sold Out",
+        url: "/cafe",
+        disabled: true
       }
     ]
   },
@@ -165,11 +177,20 @@ const PREMIER_EVENTS = [
 const EventControls = ({ controls }) => {
   return (
     <div className="event-controls">
-      {controls.map((c, i) => (
-        <a key={i} href={c.url}>
-          {c.text}
-        </a>
-      ))}
+      {controls.map((c, i) => {
+        if (c.disabled)
+          return (
+            <a key={i} className="disabled">
+              {c.text}
+            </a>
+          );
+
+        return (
+          <a key={i} href={c.url}>
+            {c.text}
+          </a>
+        );
+      })}
     </div>
   );
 };
